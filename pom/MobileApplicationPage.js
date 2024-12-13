5d5c38a239b00a6e9aa40c70c1d6530c932b126f
SvetlanaKanel <47015450+SvetlanaKanel@users.noreply.github.com>
2024-12-13 13:37:57 -0500

    change locators and clean code (#1)

--- pom/MobileApplicationPage.js
+++ pom/MobileApplicationPage.js
@@ -4,13 +4,11 @@ export default class MobileApplicationPage extends WebApplicationPage{
     constructor(page) {
         super(page);
 
-        this.pushNotificationSystemContainer = page.locator('.gap-3 > div').nth(0);
-        this.inProgressFirstContainer = page.locator('.gap-3 > div').nth(1);
         this.featureTagFromInProgressFirstTask = this.inProgressFirstContainer.getByText('Feature');
         this.highPriorityTagFromInProgressFirstTask = this.inProgressFirstContainer.getByText('High Priority');
         this.doneColumn = page.locator('.inline-flex >div').nth(3);
         this.doneListOfTasks = this.doneColumn.locator('div h3');
-        this.doneFirstContainer = page.locator('.gap-3 > div').nth(2);
+        this.doneFirstContainer = this.doneColumn.locator('.gap-3 > div').nth(0);
         this.designTagfromDoneFirstTask = this.doneFirstContainer.getByText('Design', {exact: true})
     }
 }
--- pom/WebApplicationPage.js
+++ pom/WebApplicationPage.js
@@ -6,25 +6,19 @@ export default class WebApplicationPage {
 
         this.mainHeader = page.locator('.bg-white h1');
         this.toDoColumn = page.locator('.inline-flex >div').nth(0);
-        this.toDoListOfTasks = this.toDoColumn.locator('div h3');;
-        this.toDoFirstContainer = page.locator('.gap-3 > div').nth(0);
+        this.toDoListOfTasks = this.toDoColumn.locator('div h3');
+        this.toDoFirstContainer = this.toDoColumn.locator('.gap-3 > div').nth(0);
         this.featureTagFromToDoFirstTask = this.toDoFirstContainer.getByText('Feature');
         this.highPriorityTagFromToDoFirstTask = this.toDoFirstContainer.getByText('High Priority');
-        this.toDoSecondContainer = page.locator('.gap-3 > div').nth(1);
+        this.toDoSecondContainer = this.toDoColumn.locator('.gap-3 > div').nth(1);
         this.bugTagFromToDoSecondTask = this.toDoSecondContainer.getByText('Bug', { exact: true });
 
         this.inProgressColumn = page.locator('.inline-flex >div').nth(1);
         this.inProgressListOfTasks = this.inProgressColumn.locator('div h3');
-        this.inProgressFirstContainer = page.locator('.gap-3 > div').nth(2);
-        this.designTagFromThirdTask = this.inProgressFirstContainer.getByText('Design', { exact: true });
+        this.inProgressFirstContainer = this.inProgressColumn.locator('.gap-3 > div').nth(0);
+        this.designTagFromInProgressFirstTask = this.inProgressFirstContainer.getByText('Design', { exact: true });
         this.mobileApplicationBtn = page.getByRole('button', { name: 'Mobile Application' });
-    }
-
-    async verifyToDoListHas(listItem) {
-        const toDoListText = await this.toDoListOfTasks.allInnerTexts();
-        const actualResult = toDoListText.filter((el) => el == listItem).join();
-        expect(actualResult).toEqual(listItem);
-    }
+    }  
 
     async clickMobileApplicationBtn() {
         await this.mobileApplicationBtn.click();
